38ac299eb63551c7ca7b925ba2eec0fddf94c2f7
Siguma <you8jun5@gmail.com>
2018-04-02 23:30:48 +0900

    ログを端末で確認するためのスクロールビュー作成

---
+++ Assets/Scripts/Utility/MessageScroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace utility {
+    [RequireComponent(typeof(ScrollRect))]
+    public class MessageScroll : MonoBehaviour {
+
+        private static string logs = "";
+        private string oldLogs = "";
+
+        private ScrollRect scrollRect;
+        private Text textLog;
+
+        private void Start() {
+            scrollRect = GetComponent<ScrollRect>();
+            textLog = scrollRect.content.GetComponent<Text>();
+        }
+
+        private void Update() {
+            if (logs != oldLogs) {
+                textLog.text = logs;
+                scrollRect.verticalNormalizedPosition = 0;
+                oldLogs = logs;
+            }
+        }
+
+        public static void Log(string logText) {
+            logs += (logText + "\n");
+            Debug.Log(logText);
+        }
+
+    }
+}
