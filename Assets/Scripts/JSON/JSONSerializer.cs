e7b0b06b3d1d43ada66ea2cdb821d4ff74547088
Siguma <you8jun5@gmail.com>
2018-04-03 23:52:30 +0900

    シリアライズ対象のクラスを作成

---
+++ Assets/Scripts/JSON/JSONSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace json {
+    public class JSONSerializer : MonoBehaviour {
+
+        private void Start() {
+        }
+    }
+}
---
+++ Assets/Scripts/Model/SerializeData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SerializeData {
+
+    public int testNum_;
+    public float testFloat_;
+    public string testString_;
+    public bool testBool_;
+
+    public int[] testArray_;
+    public List<string> testList_;
+    public Dictionary<int, string> testDic_;
+}
