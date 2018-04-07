ae0343e84cbfcb4e7e745358068cd9076724db5b
Siguma <you8jun5@gmail.com>
2018-04-07 15:27:11 +0900

    時間を計測し表示するためのUtilクラスを作成

---
+++ Assets/Scripts/Utility/StopWatchUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace utility {
+    public static class StopWatchUtil {
+
+        private static Stopwatch sw_ = new Stopwatch();
+
+        public static void CountStart() {
+            sw_.Restart();
+        }
+
+        public static void CountEnd() {
+            sw_.Stop();
+        }
+
+        public static string GetTimeString(string timeName) {
+            return string.Format("{0}: {1} ms", timeName, sw_.ElapsedMilliseconds);
+        }
+
+        public static void PrintTimeString(string timeName) {
+            MessageScroll.Log(GetTimeString(timeName));
+        }
+
+    }
+}
